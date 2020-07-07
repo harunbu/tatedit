b8d87cde1724a897503e8449e6aaa4fb113f6ff5
HarunobuTsutsumi <h.tsutsumi1203@gmail.com>
2020-07-08 00:44:48 +0900

    関数の位置を調整

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -10,6 +10,14 @@ const horizontalKeys:string[] = rightKeys.concat(leftKeys);
 const cursorKeys:string[] = verticalKeys.concat(horizontalKeys);
 
 /**
+ * カーソルの位置を示すオブジェクト
+ */
+interface CursorPos {
+  element: Node,
+  offset: number,
+}
+
+/**
  * キーダウンハンドラ
  * @param e
  */
@@ -21,11 +29,24 @@ function keydown(e: React.KeyboardEvent) : void {
 }
 
 /**
- * カーソルの位置を示すオブジェクト
+ * カーソルキーハンドラ
  */
-interface CursorPos {
-  element: Node,
-  offset: number,
+function cursorKeyDown(key : string) : boolean {
+  //カーソルがいる要素と位置を取得
+  const pos = getCursorPos();
+  if (pos === null) {
+    return false;
+  }
+  //移動先の要素を判定
+  const nextPos = getNextPos(key, pos);
+  if (! nextPos) {
+    return false;
+  }
+
+  //移動
+  moveCursor(nextPos);
+
+  return true;
 }
 
 /**
@@ -46,6 +67,83 @@ function getCursorPos() : CursorPos | null {
   return cursorPos;
 }
 
+/**
+ * 移動先の要素を取得
+ */
+function getNextPos(key : string, pos : CursorPos) : CursorPos | false {
+  if (upKeys.includes(key)) {
+    return upKeyDown(key, pos);
+  }
+  if (downKeys.includes(key)) {
+    return downKeyDown(key, pos);
+  }
+
+  return false;
+}
+
+/**
+ * カーソルを移動
+ * @param pos
+ */
+function moveCursor(pos : CursorPos) {
+  const range = document.createRange();
+  range.setStart(pos.element, pos.offset);
+  range.setEnd(pos.element, pos.offset);
+
+  const selection = window.getSelection();
+  selection?.removeAllRanges();
+  selection?.addRange(range);
+}
+
+/**
+ * 上キーを押された場合
+ * @param key 
+ * @param pos 
+ */
+function upKeyDown(key : string, pos : CursorPos) : CursorPos {
+  const nextPos:CursorPos = {
+    element: pos.element,
+    offset: pos.offset,
+  };
+  if (pos.offset > 0) {
+    //要素内の移動であればそのままオフセットだけずらす
+    nextPos.offset = pos.offset - 1;
+  } else {
+    //要素の一番上で押された場合は、一つ前の要素の一番下に移動する
+    const previous = getPreviousElement(pos.element);
+    if (previous) {
+      nextPos.element = previous;
+      nextPos.offset = previous.nodeValue?.length || pos.offset;
+    }
+  }
+  return nextPos;
+}
+
+/**
+ * 下キーを押された場合
+ * @param key 
+ * @param pos 
+ */
+function downKeyDown(key : string, pos : CursorPos) : CursorPos {
+  const strLength:number = pos.element.nodeValue?.length || 0;
+  const nextPos:CursorPos = {
+    element: pos.element,
+    offset: pos.offset,
+  };
+  if (pos.offset + 1 <= strLength) {
+    //要素内の移動であればそのままオフセットだけずらす
+    nextPos.offset = pos.offset + 1;
+  } else {
+    //要素の一番下で押された場合は、一つ後の要素の一番上に移動する
+    const next = getNextElement(pos.element);
+    if (next) {
+      nextPos.element = next;
+      nextPos.offset = 0;
+    }
+  }
+  return nextPos;
+}
+
 //一つ前の要素の親要素を取得する
 function getPreviousParentElement(element:Node) {
   if ((element.parentElement?.id || '') === 'edit-area') {
@@ -63,7 +161,7 @@ function getPreviousElement(element:Node) {
   return previousParentElement;
 }
 
-//一つ前の要素の親要素を取得する
+//一つ後の要素の親要素を取得する
 function getNextParentElement(element:Node) {
   if ((element.parentElement?.id || '') === 'edit-area') {
     return element.nextSibling;
@@ -71,89 +169,13 @@ function getNextParentElement(element:Node) {
   return element.parentElement?.nextSibling;
 }
 
-//一つ前の要素を取得する
+//一つ後の要素を取得する
 function getNextElement(element:Node) {
   const previousParentElement = getNextParentElement(element);
   if ((previousParentElement?.nodeType || 0) === 1) {
     return previousParentElement?.childNodes[0];
   }
   return previousParentElement;
-}
-
-//上キーを押された場合
-function upKeyDown(key : string, pos : CursorPos) : CursorPos {
-    const nextPos:CursorPos = {
-      element: pos.element,
-      offset: pos.offset,
-    };
-    if (pos.offset > 0) {
-      //要素内の移動であればそのままオフセットだけずらす
-      nextPos.offset = pos.offset - 1;
-    } else {
-      //要素の一番上で押された場合は、一つ前の要素の一番下に移動する
-      const previous = getPreviousElement(pos.element);
-      if (previous) {
-        nextPos.element = previous;
-        nextPos.offset = previous.nodeValue?.length || pos.offset;
-      }
-    }
-    return nextPos;
-}
-//下キーを押された場合
-function downKeyDown(key : string, pos : CursorPos) : CursorPos {
-    //要素内の異動であればそのままオフセットだけずらす
-    const strLength:number = pos.element.nodeValue?.length || 0;
-    const nextPos:CursorPos = {
-      element: pos.element,
-      offset: pos.offset,
-    };
-    if (pos.offset + 1 <= strLength) {
-      //要素内の移動であればそのままオフセットだけずらす
-      nextPos.offset = pos.offset + 1;
-    } else {
-      //要素の一番下で押された場合は、一つ後の要素の一番上に移動する
-      const next = getNextElement(pos.element);
-      if (next) {
-        nextPos.element = next;
-        nextPos.offset = 0;
-      }
-    }
-    return nextPos;
-}
-
-/**
- * カーソルを移動
- * @param pos
- */
-function moveCursor(pos:CursorPos) {
-  //console.log(pos);
-  const range = document.createRange();
-  range.setStart(pos.element, pos.offset);
-  range.setEnd(pos.element, pos.offset);
-
-  const selection = window.getSelection();
-  selection?.removeAllRanges();
-  selection?.addRange(range);
-}
-
-
-
-/**
- * カーソルキーハンドラ
- */
-function cursorKeyDown(key : string) : boolean {
-  //カーソルがいる要素と位置を取得
-  const pos = getCursorPos();
-  if (pos === null) {
-    return false;
-  }
-  //移動先の要素を判定し、移動
-  if (upKeys.includes(key)) {
-    moveCursor(upKeyDown(key, pos));
-  } else if (downKeys.includes(key)) {
-    moveCursor(downKeyDown(key, pos));
-  }
-  return true;
 }
 
 function Editor() {
